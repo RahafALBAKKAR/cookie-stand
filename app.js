e28606934f7cfab96f6f814e49a985c139bb666f
rahafalbakkar <rahaf.a7med1991@gmail.com>
2021-08-03 09:42:08 +0300

    update style

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 'use strict';
 let hours = ['6am', '7am', '8am', '9am', '10am', '11am', '12pm', '1pm', '2pm', '3pm', '4pm', '5pm', '6pm', '7pm'];
 const shop = document.getElementById('shop');
-const img =document.getElementById('img');
+
 const seattle = {
     name: 'seattle',
     minCust: 23,
